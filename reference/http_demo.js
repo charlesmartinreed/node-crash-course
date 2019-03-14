65852daf64b94ca556e42420bf6c6b2ed97f3e8e
charlesmartinreed <charlesmartinreed@icloud.com>
2019-03-14 18:48:03 -0500

    Basic demonstration of node.js http module.

---
+++ reference/http_demo.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+// Create a server object
+http.createServer((req, resp) => {
+	// if we get a request, make a response
+	// response writes to the page
+	resp.write('Hello World');
+	resp.end();
+	// on port 5000
+}).listen(5000, () => console.log('server running...'));
