45cf1ba8e4c62f503aff969a5752cccc611dcf44
charlesmartinreed <charlesmartinreed@icloud.com>
2019-03-14 19:32:50 -0500

    Demonstrate request and response handling with http.

--- index.js
+++ index.js
@@ -19,32 +19,54 @@ const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((req, resp) => {
-	// loading a file
-	if(req.url === '/') {
-		fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
-			if(err) throw err;
-			resp.writeHead(200, {'Content-Type': 'text/html'});
-			resp.end(content);
-		 })
+	// build our file path dynamically
+	let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+
+	// grab the extension of the file, content type set based upon file type
+	let extname = path.extname(filePath);
+
+	//set initial content type
+	let contentType = 'text/html';
+
+	// check ext and set content type
+	switch(extname) {
+		case '.js':
+			contentType = 'text/javascript';
+			break;
+		case '.css':
+			contentType = 'text/css';
+			break;
+		case '.json':
+			contentType = 'application/json';
+			break;
+		case '.png':
+			contentType = 'image/png';
+			break;
+		case '.jpg':
+			contentType = 'image/jpg';
+			break;
 	}
 
-	if(req.url === '/about') {
-		fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
-			if(err) throw err;
-			resp.writeHead(200, {'Content-Type': 'text/html'});
-			resp.end(content);
-		 })
-	}
-
-	if (req.url === '/api/users') {
-		const users = [
-			{ name: 'Bob Smith', age: 40 },
-			{ name: 'Jane Smith', age: 43 },
-			{ name: 'Terrence Smith', age: 21 }
-		];
-		resp.writeHead(200, { 'Content-Type': 'application/json' });
-		resp.end(JSON.stringify(users));
-	}
+	// read the file
+	fs.readFile(filePath, (err, content) => {
+		if(err) {
+			if(err.code == 'ENOENT') {
+				// page not found
+				fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+					resp.writeHead(200, { 'Content-Type': 'text/html'});
+					resp.end(content, 'utf8');
+				})
+			} else {
+				// Some sort of server error
+				resp.writeHead(500);
+				resp.end(`Server Error: ${err.code}`);
+			}
+		} else {
+			// success
+			resp.writeHead(200, { 'Content-Type': contentType });
+			resp.end(content, 'utf8');
+		}
+	});
 });
 
 // using the environment variable to determine the port to listen on
